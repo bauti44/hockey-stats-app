681638cb5579d00852629bc95d70c3455e5b1979
Juan Bautista Blasco <bauti44@Juans-MacBook-Pro.local>
2020-05-28 22:19:38 -0300

    New server

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -6,7 +6,7 @@ let config = {
 }
 
 if (NODE_ENV === 'production') {
-  config.url.api = 'http://ec2-54-198-146-118.compute-1.amazonaws.com/' // Change this URL according to your live server
+  config.url.api = 'http://ec2-54-82-119-201.compute-1.amazonaws.com:8080/' // Change this URL according to your live server
 } else {
   config.url.api = '/'
 }
