de41b85d4d62a5c205410141f7ee77df22eb0377
Juan Bautista Blasco <bauti44@Juans-MacBook-Pro.local>
2020-06-01 23:01:41 -0300

    WIP

--- frontend/src/components/Menu/Menu.js
+++ frontend/src/components/Menu/Menu.js
@@ -9,6 +9,12 @@ import {
   IonIcon,
   IonButton,
   IonAlert,
+  IonItem,
+  IonMenu,
+  IonContent,
+  IonList,
+  IonMenuButton,
+  IonMenuToggle,
 } from '@ionic/react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types'
@@ -34,31 +40,54 @@ class Menu extends Component {
   render() {
     const { isAuthenticated } = this.props.user
     return (
-      <IonHeader>
-        <IonToolbar color="darkBlue">
-          {isAuthenticated ?
-            <>
-              <IonButtons slot="start">
-                <IonBackButton defaultHref={URL_REPO.ROOT} onClick={this.goBack.bind(this)} />
-              </IonButtons>
-              <IonButtons slot="end">
-                <IonButton shape="round" slot="icon-only">
-                  <a href={URL_REPO.MATCH_LIST}>
-                    <IonIcon size="large" name="list" />
-                  </a>
-                </IonButton>
-              </IonButtons>
-            </> : <></>}
-          <IonTitle onClick={this.onTitleClick.bind(this)}>Hockey Stats App</IonTitle>
-        </IonToolbar>
-        <IonAlert
-          isOpen={this.state.showAlert}
-          onDidDismiss={() => this.setState({ showAlert: false })}
-          header={"Hockey Stats App"}
-          message={"Author: <a href='mailto:bauti44@gmail.com'>@bauti44</a><br>Version: 1.0<br>"}
-          buttons={['OK']}
+      <>
+        <IonMenu menuId="barMenu" contentId="main" side="start">
+          <IonHeader>
+            <IonToolbar color="danger">
+              <IonTitle>End Menu</IonTitle>
+            </IonToolbar>
+          </IonHeader>
+          <IonContent>
+            <IonList>
+              <IonItem>Menu Item</IonItem>
+              <IonItem>Menu Item</IonItem>
+              <IonItem>Menu Item</IonItem>
+              <IonItem>Menu Item</IonItem>
+              <IonItem>Menu Item</IonItem>
+            </IonList>
+          </IonContent>
+        </IonMenu>
+        <IonHeader>
+          <IonToolbar color="darkBlue">
+            {isAuthenticated ?
+              <>
+                <IonButtons slot="start">
+                  <IonBackButton defaultHref={URL_REPO.ROOT} onClick={this.goBack.bind(this)} />
+                </IonButtons>
+                <IonButtons slot="end">
+                  <IonButton shape="round" slot="icon-only">
+                    <a href={URL_REPO.MATCH_LIST}>
+                      <IonIcon size="large" name="list" />
+                    </a>
+                  </IonButton>
+                  <IonMenuButton menu="barMenu" slot="end">
+                    <IonButton shape="round" slot="icon-only">
+                      <IonIcon size="large" name="menu" />
+                    </IonButton>
+                  </IonMenuButton>
+                </IonButtons>
+              </> : <></>}
+            <IonTitle onClick={this.onTitleClick.bind(this)}>Hockey Stats App</IonTitle>
+          </IonToolbar>
+          <IonAlert
+            isOpen={this.state.showAlert}
+            onDidDismiss={() => this.setState({ showAlert: false })}
+            header={"Hockey Stats App"}
+            message={"Author: <a href='mailto:bauti44@gmail.com'>@bauti44</a><br>Version: 1.0<br>"}
+            buttons={['OK']}
           />
-      </IonHeader>
+        </IonHeader>
+      </>
     );
   }
 }
